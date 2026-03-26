6a0029bcf6e23beaf81b7fcbe06d74ccb6caff79
charleschiu075 <ccharles0705@gmail.com>
2026-03-26 14:35:10 +0800

    KAN-9 two sum array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
+
+vector<int> twoSum(const vector<int>& nums, int target) {
+    unordered_map<int, int> seenIndex;
+
+    for (int i = 0; i < static_cast<int>(nums.size()); i++) {
+        int complement = target - nums[i];
+
+        if (seenIndex.find(complement) != seenIndex.end()) {
+            return {seenIndex[complement], i};
+        }
+
+        seenIndex[nums[i]] = i;
+    }
+
+    return {};
+}
 
 int main() {
     int n;
@@ -18,15 +35,12 @@ int main() {
     cout << "Enter the target number: ";
     cin >> target;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] == target) {
-                cout << "[" << i << ", " << j << "]\n";
-                return 0;
-            }
-        }
+    vector<int> answer = twoSum(nums, target);
+    if (!answer.empty()) {
+        cout << "[" << answer[0] << ", " << answer[1] << "]\n";
+    } else {
+        cout << "No valid pair found.\n";
     }
 
-    cout << "No valid pair found.\n";
     return 0;
 }
